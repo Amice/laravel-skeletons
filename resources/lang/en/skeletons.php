e8f0a95e5149ce55572b98df58f91c51043e4d7d
Kovács László <149382301+kovacs-laci@users.noreply.github.com>
2025-03-18 14:00:21 +0100

    adding localization

--- resources/lang/en/skeletons.php
+++ resources/lang/en/skeletons.php
@@ -5,4 +5,12 @@ return [
     'deleted_successfully' => ':Model deleted successfully.',
     'name_max_length' => 'The name may not be greater than 255 characters.',
     'name_required' => 'The name field is required.',
+    'create_new' => 'New',
+    'name' => 'Name',
+    'save' => 'Save',
+    'cancel' => 'Cancel',
+    'actions' => 'Actions',
+    'delete' => 'Delete',
+    'details_of' => 'Details of',
+    'back' => 'Back',
 ];
--- resources/lang/hu/skeletons.php
+++ resources/lang/hu/skeletons.php
@@ -5,4 +5,12 @@ return [
     'deleted_successfully' => ':Model sikeresen törölve.',
     'name_max_length' => 'A megnevezés mező hossza legfeljebb 255 karakter lehet.',
     'name_required' => 'A megnevezés mező kitöltése kötelező.',
+    'create_new' => 'Új',
+    'name' => 'Név',
+    'save' => 'Ment',
+    'cancel' => 'Mégse',
+    'actions' => 'Műveletek',
+    'delete' => 'Töröl',
+    'details_of' => 'Részletek',
+    'back' => 'Vissza',
 ];
